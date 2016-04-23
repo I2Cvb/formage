ce2579ed12f5ede1fa6ba9fed46f5b678c8647c3
Joan Massich <sik@visor.udg.edu>
2016-04-23 19:34:34 +0200

    Added parametric test

--- src/test_test.cpp
+++ src/test_test.cpp
@@ -2,6 +2,20 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <gtest/gtest.h>
+#include <tuple>
+
+using param_struct_t = std::tuple<std::string, bool>;
+
+class IntegrationTestFixture : public ::testing::TestWithParam<param_struct_t>
+                             //, public ::testing::Test
+{
+     protected:
+        virtual void setUp()
+        {}
+        virtual void tearDown()
+        {}
+};
+
 
 bool isNicelySealed(const cv::Mat &cheese_img)
 {
@@ -26,6 +40,27 @@ static std::string getTestDataPath()
 
 }
 
+TEST_P(IntegrationTestFixture, testImage)
+{
+    std::string path;
+    bool expectedOutput;
+    auto param = GetParam();
+    std::tie<std::string,bool>(path, expectedOutput) = param;
+ 
+    /* cv::Mat img; */
+    /* ASSERT_NO_THROW(img = cv::imread(getTestDataPath() + path) << "Hi ha hagut una excepcio"); */
+
+    /* EXPECT_TRUE(img.cols > 0) << "L'imatge probablement esta vuida!!!"; */
+    /* EXPECT_TRUE(img.rows > 0) << "L'imatge probablement esta vuida!!!"; */
+    /* EXPECT_FALSE(isNicelySealed(img)); */
+ 
+}
+
+INSTANTIATE_TEST_CASE_P(InstaName,
+                        IntegrationTestFixture,
+                        ::testing::Values(param_struct_t{"xx.png", false}, 
+                                          param_struct_t{"yy.png", true}));
+
 TEST(test_case_name, test_name)
 {
     cv::Mat img;
@@ -42,3 +77,4 @@ int main(int ac, char* av[])
     testing::InitGoogleTest(&ac, av);
     return RUN_ALL_TESTS();
 }
+
