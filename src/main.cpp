156ab6ec4586182cc35fc41813ad90b6738614b9
Joan Massich <sik@visor.udg.edu>
2016-04-23 18:10:44 +0200

    load a simple image in OpenCU

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 
 #include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 int main()
 {
-    cv::Mat a;
+    cv::Mat a = cv::imread("../../testdata/xx.png");
+
+    cv::imshow("hola", a);
+    cv::waitKey(0);
 
     return 0;
 }
