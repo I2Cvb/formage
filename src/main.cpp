eb42d7fe5a3e95eadc00e4cf01e67e18ded5d4dc
Joan Massich <sik@visor.udg.edu>
2016-05-09 14:18:30 +0200

    something

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,12 @@
 int main()
 {
     // Image load
-    cv::Mat a = cv::imread("../../testdata/A/A05_38_gt.png", CV_LOAD_IMAGE_UNCHANGED);
+    cv::Mat a = cv::imread("../../testdata/A/A05_38_gt_gray.png", CV_LOAD_IMAGE_GRAYSCALE);
     cv::resize(a, a, cv::Size(), 0.1, 0.1);
 
     std::cout << a.size() <<std::endl;
     cv::Mat b(a, cv::Rect(10,10,20,20));
-    std::cout << a.data << std::endl << std::endl;
+    std::cout << b << std::endl << std::endl;
 
     cv::imshow("hola", b);
     cv::waitKey(0);
