ff74afa8273a3354bcddcd39cf735a27271a480b
Joan Massich <sik@visor.udg.edu>
2016-05-19 09:16:45 +0200

    Added a dummy example

---
+++ src/apps/dummy.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class MyData
+{
+  public:
+    const int foo;
+    int bar;
+    MyData(const int& _initBar ) : foo(_initBar)
+    {
+        bar = _initBar; //some cool operation to set up bar
+    }
+    void update(const int& _newbar) { bar = _newbar;};
+    string get(){ return "foo: " + to_string(foo) + " bar: " + to_string(bar);};
+
+};
+
+class A
+{
+ protected:
+    const string s;
+    MyData d;
+
+ public:
+    A( auto _s, int& _i ): s( _s ), d( _i ) { cout << "Constructor A: " << s <<endl; };
+    virtual ~A(void) { cout << "destructor A" << endl; };
+    virtual void f() { cout << "A::f()" << endl; };
+    virtual void g() { cout << "A::g()" << endl; };
+    virtual string get(){ return "s: " + s + "d: (" + d.get() +")";};
+};
+
+
+class BA : public A
+{
+  protected:
+    virtual string k() = 0;
+  public:
+    BA( auto _s, auto _i) : A( _s, _i) { cout << "Constructor B: " << endl; };
+    void h() { cout << "B::h()" << endl; };
+};
+
+class CBA : public BA
+{
+    int foobar;
+  public:
+    CBA( auto _s, auto _i ) : BA( _s, _i ), foobar(42) { l(); };
+    CBA( auto _s, auto _i, auto _fb ) : BA( _s, _i ), foobar(_fb) { l(); };
+  private:
+    void l() { cout << "CBA::l()" << endl; };
+};
+
+class DA : public A
+{
+    vector<string> s_vec;
+    BA* b_ptr;
+public:
+    DA( auto _s, auto _i ) : A ( _s,  _i ) {};
+    void m() { cout << "DA::m()" << endl; };
+    ~DA(){ cout << "~DA" <<endl; };
+};
+
+void show(const string who, const string info)
+{
+    cout << "MAIN:: "+ who + info << endl;
+}
+int main( int argc, const char** argv )
+{
+    MyData d(33);
+    show("d", d.get());
+    d.update(99);
+    show("d", d.get());
+
+    CBA cba("cbaName", 100);
+    show("cba", cba.get());
+    DA da("cbaName", 200);
+    show("da", da.get());
+}
+
+
